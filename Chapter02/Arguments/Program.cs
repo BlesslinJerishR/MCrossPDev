1442dffbbd2ffc5f409d2d35e64690d2aa979369
BlesslinJerishR <mastermindx33@gmail.com>
2022-03-23 16:59:26 +0530

    deploy @ Exercises

---
+++ Chapter02/Arguments/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using static System.Console;
+
+namespace Arguments{
+    class Program{
+        static void Main(string[] args)
+        {
+            WriteLine("This Program is working");
+            WriteLine($"There are {args.Length} arguments");
+            int num = 1;
+            foreach (string arg in args){
+                WriteLine($"Argument {num} : {arg}");   
+                num += 1;
+            }
+
+            if(args.Length < 4){
+                WriteLine("You must specify 2 colors and Dimensions, eg : ");
+                WriteLine("dotnet run red yellow 80 40");
+                return; //stop running
+            }
+
+            ForegroundColor = (ConsoleColor)Enum.Parse(
+                enumType: typeof(ConsoleColor),
+                value: args[0],
+                ignoreCase: true
+            );
+
+            BackgroundColor = (ConsoleColor)Enum.Parse(
+                enumType: typeof(ConsoleColor),
+                value: args[1],
+                ignoreCase: true
+            );
+
+            try{
+                WindowWidth = int.Parse(args[2]);
+                WindowHeight = int.Parse(args[3]);
+            }
+            catch(PlatformNotSupportedException){
+                WriteLine("The Current platform does not support changing the size of the console window");
+            }
+            
+        }
+    }
+}
---
+++ Chapter02/Fishing/Program.cs
@@ -0,0 +1,31 @@
+﻿using static System.Console;
+// Telephone number
+long phoneNumber = 8989898989;
+WriteLine($"Phone number : {phoneNumber}");
+// Height
+decimal height = 1.77M;
+WriteLine($"Height : {height}");
+// Age
+int age = 24;
+WriteLine($"Age : {age}");
+// Salary
+int salary = 25_000;
+WriteLine($"Salary : {salary}");
+// International standard book number
+long isbn = 912345667433;
+WriteLine($"Isbn : {isbn}");
+// Book's Price
+int bookPrice = 499;
+WriteLine($"Book Price : {bookPrice}");
+// Shipping weight
+decimal weight = 8.4M;
+WriteLine($"Weight : {weight}");
+// Country's population
+long population = 9_888_899;
+WriteLine($"Country's population : {population}");
+// Stars in universe
+long stars = 789_888;
+WriteLine($"Stars in universe : {stars}");
+// Number of Employees
+int employees = 9000;
+WriteLine($"Number of Employees : {employees}");
